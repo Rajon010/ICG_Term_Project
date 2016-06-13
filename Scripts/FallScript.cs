db974616f4bc9653f7e48f02213b03817d9cc2f7
Rajon010 <b03902010@ntu.edu.tw>
2016-06-13 21:02:15 +0800

    camera falling after shot

--- Scripts/FallScript.cs
+++ Scripts/FallScript.cs
@@ -4,14 +4,34 @@ using System.Collections;
 public class FallScript : MonoBehaviour {
 
 	public bool shot = false;
+	// if shot is true, then move pos from y = 0.25 (0) to -0.43 (-0.68)
+
+	private int BEFORE_FALL = 0;
+	private int FALLING = 1;
+	private int AFTER_FALL = 2;
+	private int state;
+	private float g = -0.003F;
+	private float speed = 0;
+	private float delta_y = 0;
+	//private float origin_y;
 
 	// Use this for initialization
 	void Start () {
-	
+		state = BEFORE_FALL;
+		//origin_y = transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (shot == true && state == BEFORE_FALL)
+			state = FALLING;
+		if (state == FALLING) {
+			speed += g;
+			delta_y += speed;
+			if (delta_y >= -2.75)
+				transform.position = transform.position + new Vector3 (0, speed, 0);
+			else
+				state = AFTER_FALL;
+		}
 	}
 }
