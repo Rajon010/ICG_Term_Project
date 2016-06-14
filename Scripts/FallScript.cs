b10d6066c0203bd3d10cfb20d380ee98cae1e7ce
unknown <Rajon010>
2016-06-14 11:05:04 +0800

    fall and resume, nice

--- Scripts/FallScript.cs
+++ Scripts/FallScript.cs
@@ -4,34 +4,53 @@ using System.Collections;
 public class FallScript : MonoBehaviour {
 
 	public bool shot = false;
+	public bool ready_resume = false;
 
 	private int BEFORE_FALL = 0;
 	private int FALLING = 1;
 	private int AFTER_FALL = 2;
+	private int RESUME = 3;
 	private int state;
 	private float g = -0.003F;
 	private float speed = 0;
 	private float delta_y = 0;
-	//private float origin_y;
+	private float origin_y;
+	private int tick = 0;
 
 	// Use this for initialization
 	void Start () {
 		state = BEFORE_FALL;
-		//origin_y = transform.position.y;
+		origin_y = transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (shot && state == BEFORE_FALL)
+		if (shot && state == BEFORE_FALL) {
 			state = FALLING;
-		else if (state == FALLING) {
+		}else if (state == FALLING) {
 			speed += g;
 			delta_y += speed;
 			if (delta_y >= -2.75)
 				transform.position = transform.position + new Vector3 (0, speed, 0);
-			else
+			else {
 				state = AFTER_FALL;
-		}//else if(state == AFTER_FALL)
-			
+				tick = 0;
+			}
+		} else if (state == AFTER_FALL) {
+			tick++;
+			if (tick == 70) {
+				GameObject.Find ("FPSController").GetComponent<PlayerScript> ().resume = true;
+				state = RESUME;
+			}
+		} else if (ready_resume && state == RESUME) {
+			float x = transform.position.x;
+			float z = transform.position.z;
+			transform.position = new Vector3 (x, origin_y, z);
+			ready_resume = false;
+			shot = false;
+			state = BEFORE_FALL;
+			delta_y = 0;
+			speed = 0;
+		}
 	}
 }
--- Scripts/PlayerScript.cs
+++ Scripts/PlayerScript.cs
@@ -4,20 +4,42 @@ using System.Collections;
 public class PlayerScript : MonoBehaviour {
 
 	public bool shot = false;
-	public bool resume = false;
+	public bool resume = false; // whether camera has finished falling
 
 	private bool tell_camera = false;
+	private Vector3[] record_pos = new Vector3[2];
+	private Quaternion[] record_rotation = new Quaternion[2];
+	private int tick = 0;
+	private int index = 0;
 
 	// Use this for initialization
 	void Start () {
-	
+		record_pos [0] = record_pos [1] = transform.position;
+		record_rotation [0] = record_rotation [1] = transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		tick++;
 		if (!tell_camera && shot) {
 			GameObject.Find ("FirstPersonCharacter").GetComponent<FallScript> ().shot = true;
 			tell_camera = true;
+		} else if (resume) {
+			transform.position = record_pos [index];
+			transform.rotation = record_rotation [index];
+			GameObject.Find ("FirstPersonCharacter").GetComponent<FallScript> ().ready_resume = true;
+			record_pos [1 - index] = record_pos [index];
+			record_rotation [1 - index] = record_rotation [index];
+			shot = false;
+			resume = false;
+			tell_camera = false;
+			tick = 0;
+		}
+		if (!shot && tick == 30) {
+			record_pos [index] = transform.position;
+			record_rotation [index] = transform.rotation;
+			index = 1 - index;
+			tick = 0;
 		}
 	}
 }
